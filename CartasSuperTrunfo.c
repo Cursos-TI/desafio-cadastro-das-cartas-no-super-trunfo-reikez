8d69202ca7b2bdfc9888b71fcfdad68e13baa71e
reikez <reikedelucas2@HOTMAIL.COM>
2025-02-18 08:33:41 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
 
 int main() {
-printf("desafio de xadrez!\n");
-printf("novo commit\n")
+    struct Carta cartas[100]; // Array para armazenar até 100 cartas
+    int quantidade = 0; // Número de cartas cadastradas
+    int opcao;
+
+    do {
+        printf("\n===== MENU SUPER TRUNFO - PAISES =====\n");
+        printf("1. Cadastrar cartas\n");
+        printf("2. Exibir cartas\n");
+        printf("3. Sair\n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &opcao);
+
+        switch (opcao) {
+            case 1:
+                cadastrarCartas(cartas, &quantidade);
+                break;
+            case 2:
+                exibirCartas(cartas, quantidade);
+                break;
+            case 3:
+                printf("\nSaindo do programa. Até mais!\n");
+                break;
+            default:
+                printf("\nOpção inválida! Tente novamente.\n");
+        }
+    } while (opcao != 3);
 
     return 0;
-}
